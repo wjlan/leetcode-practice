48df236b4609f1c03d5e41670e9cd11124bfd40f
wanjun <wanjunlan@gmail.com>
2024-02-28 18:44:08 -0800

    updated

--- src/1-two_sum.py
+++ src/1-two_sum.py
@@ -1,10 +1,10 @@
-# Given an array of integers `nums` and an integer `target`, return *indices of the two numbers such that they add up to `target`*.
+# Given an array of integers `nums` and an integer `target`, return *indices of the two numbers such that they add up to `target`.
 
 # You may assume that each input would have ***exactly* one solution**, and you may not use the *same* element twice.
 
 # You can return the answer in any order.
 
-# **Example 1:**
+# Example 1:
 
 # ```
 # Input: nums = [2,7,11,15], target = 9
@@ -13,7 +13,7 @@
 
 # ```
 
-# **Example 2:**
+# Example 2:
 
 # ```
 # Input: nums = [3,2,4], target = 6
@@ -21,7 +21,7 @@
 
 # ```
 
-# **Example 3:**
+# Example 3:
 # Input: nums = [3,3], target = 6
 # Output: [0,1]
 
