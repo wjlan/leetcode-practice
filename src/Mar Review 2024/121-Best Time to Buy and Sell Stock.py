e6ff69549fd37974b146db94f52be38773dc8fb2
wanjun <wanjunlan@gmail.com>
2024-03-08 16:24:44 -0800

    u

---
+++ src/Mar Review 2024/121-Best Time to Buy and Sell Stock.py
@@ -0,0 +1,59 @@
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+# Example 1:
+
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+# Example 2:
+
+# Input: prices = [7,6,4,3,1]
+# Output: 0
+# Explanation: In this case, no transactions are done and the max profit = 0.
+
+
+# 打擂台
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        lowest = prices[0]
+        for price in prices:
+            if price > lowest:
+                max_profit = max(max_profit, price - lowest)
+            else:
+                lowest = price
+
+        return max_profit
+    
+
+# 同向双指针
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                maxP = max(maxP, prices[r] - prices[l])
+            else:
+                l = r
+            r += 1
+        
+        return maxP
+    
+
+# Kadane算法
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_ending_here = 0
+        max_profit = 0
+        for i in range(1, len(prices)):
+            max_ending_here = max(0, max_ending_here + prices[i] - prices[i - 1])
+            max_profit = max(max_profit, max_ending_here)
+        return max_profit
